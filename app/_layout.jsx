2a1ccbbd808aa90a9d4112aa6a6acdc536e8372a
Fatos Rama <fatosrama.fr@gmail.com>
2025-10-03 14:20:10 +0200

    Added a black backgorund of the index layout and added a test page which can be redirected by pressing the button 'Back'

---
+++ app/_layout.jsx
@@ -0,0 +1,30 @@
+import {Stack } from "expo-router";
+import {Pressable, Text} from "react-native";
+import {useRouter} from "expo-router";
+
+export default function RootLayout() {
+    const router = useRouter();
+    return ( 
+        <Stack>
+            <Stack.Screen name="index"
+            
+            options = {{
+                title: "Home Page",
+                headerStyle: {
+                    backgroundColor: "#121212",
+                },
+                headerTintColor: "#fff",
+                headerLeft: () => (
+                    <Pressable onPress= {() => router.push("/testpage")}>
+                        <Text style={{color: "white",marginLeft:10}}>Back</Text>
+                    </Pressable>
+                ), 
+                
+            }}
+            
+            
+            
+            />
+        </Stack>
+    );
+}
--- app/testpage.jsx
+++ app/testpage.jsx
@@ -1,6 +1,6 @@
 import {Text, View} from "react-native";
 
-export default function Index() {
+export default function TestPage() {
     return (
         <View 
         style={{
@@ -9,7 +9,7 @@ export default function Index() {
             alignItems: "center",
         }}
         >
-            <Text>Hello World!</Text>
+            <Text>Hello World and welcome to the Test Page!</Text>
         </View>
 
     );

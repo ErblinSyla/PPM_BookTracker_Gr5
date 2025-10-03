0e3c42a667f31fe9931832b6875ec082ceb266be
eraberisha <era.berisha5@student.uni-pr.edu>
2025-10-03 19:09:40 +0200

    Added login screen with signup navigation

---
+++ app/login.jsx
@@ -0,0 +1,55 @@
+import { View, Text, TextInput, Button, Pressable } from "react-native";
+import { useRouter } from "expo-router";
+
+export default function Login() {
+  const router = useRouter();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+        backgroundColor: "#e0f7fa",
+      }}
+    >
+      <Text style={{ fontSize: 24, marginBottom: 20 }}>Log In</Text>
+
+      <TextInput
+        placeholder="Username or Email"
+        style={{
+          width: "100%",
+          height: 50,
+          borderColor: "#ccc",
+          borderWidth: 1,
+          borderRadius: 8,
+          marginBottom: 10,
+          paddingLeft: 10,
+        }}
+      />
+
+      <TextInput
+        placeholder="Password"
+        secureTextEntry={true}
+        style={{
+          width: "100%",
+          height: 50,
+          borderColor: "#ccc",
+          borderWidth: 1,
+          borderRadius: 8,
+          marginBottom: 20,
+          paddingLeft: 10,
+        }}
+      />
+
+      <Button title="Log In" onPress={() => {}} />
+
+      <Pressable onPress={() => router.push("/signup")}>
+        <Text style={{ color: "blue", marginTop: 20 }}>
+          Don't have an account? Sign Up
+        </Text>
+      </Pressable>
+    </View>
+  );
+}

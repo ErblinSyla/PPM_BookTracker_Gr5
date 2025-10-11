07d26fce7a6be3e380062994c2cdf14aca266899
Ereza Greicevci <ereza.greicevci@student.uni-pr.edu>
2025-10-11 19:03:09 +0200

    Update signup.jsx
    Added account creation buttons

--- app/signup.jsx
+++ app/signup.jsx
@@ -1,9 +1,25 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 export default function CreateAcc() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Create an Account</Text>
+
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Continue with Email</Text>
+      </Pressable>
+
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Continue with Google</Text>
+      </Pressable>
+
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Continue with Apple</Text>
+      </Pressable>
+
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Continue with SSO</Text>
+      </Pressable>
     </View>
   );
 }
@@ -21,5 +37,18 @@ const styles = StyleSheet.create({
     fontSize: 26,
     fontWeight: "bold",
     marginBottom: 30,
+  },
+  button: {
+    width: "100%",
+    paddingVertical: 14,
+    borderWidth: 1,
+    borderColor: "#fff",
+    borderRadius: 25,
+    marginBottom: 15,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 16,
   },
 });

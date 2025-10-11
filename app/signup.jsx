d3b394e20ab2d3b5d6c2e576126cb076f8814846
Ereza Greicevci <ereza.greicevci@student.uni-pr.edu>
2025-10-11 19:47:32 +0200

    Update signup.jsx

--- app/signup.jsx
+++ app/signup.jsx
@@ -32,7 +32,7 @@ export default function CreateAcc() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#0d1b2a", // ngjyra e backgroundit 
+    backgroundColor: "#0d1b2a", // navy blue
     justifyContent: "center",
     alignItems: "center",
     padding: 24,

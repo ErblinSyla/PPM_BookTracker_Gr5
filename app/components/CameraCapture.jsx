4b26f0b4832d3c2d363645f2adfaf3898218430a
Fatos Rama <fatosrama.fr@gmail.com>
2025-12-15 18:39:36 +0100

    Added a Camera Capture component

---
+++ app/components/CameraCapture.jsx
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { CameraView } from "expo-camera";
+import styles from "../screens/styles/AddNewBookStyles";
+
+const CameraCapture = ({ onBack, onCapture }) => {
+  const cameraRef = useRef(null);
+
+  return (
+    <View style={styles.cameraContainer}>
+      <CameraView style={styles.camera} ref={cameraRef} facing="back" />
+      <View style={styles.cameraOverlay}>
+        <TouchableOpacity onPress={onBack}>
+          <Text style={{ fontSize: 24, color: "white", fontWeight: "700" }}>
+            Back
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={async () => cameraRef.current && onCapture(await cameraRef.current.takePictureAsync({ quality: 0.8 }))}
+        >
+          <View style={styles.captureBtn} />
+        </TouchableOpacity>
+        <View style={{ width: 60 }} />
+      </View>
+    </View>
+  );
+};
+
+export default React.memo(CameraCapture);

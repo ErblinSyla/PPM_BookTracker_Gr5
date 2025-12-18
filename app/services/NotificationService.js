50935c144153ae7c5999f8ec6fa27dd4023a8f9a
Fatos Rama <fatosrama.fr@gmail.com>
2025-12-18 19:33:31 +0100

    Installed expo-notifications and expo-device and added a notification handler in the service

---
+++ app/services/NotificationService.js
@@ -0,0 +1,11 @@
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+import { Platform } from 'react-native';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
+});

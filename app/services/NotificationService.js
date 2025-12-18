7b98b5214e9ccc0b03473f6293adf66d89de31d7
Fatos Rama <fatosrama.fr@gmail.com>
2025-12-18 20:37:43 +0100

    Added a Push Notification Test function

--- app/services/NotificationService.js
+++ app/services/NotificationService.js
@@ -24,3 +24,33 @@ async function requestPermissions() {
     }
     return finalStatus === 'granted';
 }
+
+async function sendTestNotification(){
+    const ok = await requestPermissions();
+    if (!ok) throw new Error('Push notification permission not granted');
+
+    if (Platform.OS === 'android') {
+        await Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.DEFAULT,
+        });
+    }
+}
+
+return Notifications.scheduleNotificationAsync({
+    content: {
+        title: "Test Notification",
+        body: 'This is a test notification from NotificationService',
+        data: { test: true },
+    },
+    trigger: {
+        type: 'timeInterval',
+        seconds: 1,
+        repeats: false, 
+        },
+});
+
+export default {
+    sendTestNotification,
+    requestPermissions,
+};

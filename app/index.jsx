c73e1143553d2f25778720bddceb7a34537b7532
Erblin Syla <erblin.sylaa@gmail.com>
2025-12-20 19:55:43 +0100

    Quick fix

--- app/index.jsx
+++ app/index.jsx
@@ -35,8 +35,6 @@ export default function Index() {
 
         if (user.emailVerified) {
           router.replace("/homepage");
-        } else {
-          router.replace("/login");
         }
       }
     });

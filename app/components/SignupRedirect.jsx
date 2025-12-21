df44ee20bfea4e6f11497c42276c32ce6bac248a
Erblin Syla <erblin.sylaa@gmail.com>
2025-12-21 11:46:08 +0100

    Quick link fix

--- app/components/SignupRedirect.jsx
+++ app/components/SignupRedirect.jsx
@@ -3,7 +3,7 @@ import { TouchableOpacity, Text } from "react-native";
 import styles from "../styles/LoginStyles";
 
 const SignupRedirect = React.memo(({ router }) => (
-  <TouchableOpacity onPress={() => router.push("/signup")}>
+  <TouchableOpacity onPress={() => router.push("/Signup")}>
     <Text style={styles.signupText}>
       Don’t have an account? <Text style={{ fontWeight: "700" }}>Sign Up</Text>
     </Text>
